c18892da528fe88c7ccacb73d142dd09860ac69e
VW Freeh <vwfreeh@gmail.com>
2019-07-10 16:59:49 -0400

    added triads and caged

--- fret.py
+++ fret.py
@@ -38,7 +38,7 @@ def getNotes(pnotes, offset, frets):
     return notes
 
 
-def run(args):
+def showNotes(args):
     print("|".join([' %-2d ' % (i, ) for i in range(args.frets)]))
     print('+'.join(['-'*4]*args.frets))
 
@@ -60,6 +60,147 @@ def run(args):
     # E string again
     print("|".join([' {:2s} '.format(note) for note in eNotes]))
 
+
+def showTriads(root_name):
+    root = (Notes.index(root_name) + 5) % 12 # set for the lo E string
+    a = (root + 9) % 12
+    e = (root + 1) % 12
+    d = (root + 4) % 12
+    if a == 0: a = 12
+    if e == 0: e = 12
+    if d == 0: d = 12
+    frets = max(12, a, e+1, d+1) + 1
+
+    print('Triads for {}'.format(root_name))
+    fret_str = ["%-2d" % (i, ) for i in range(frets)]
+    fret_str[root] = root_name + " "
+    print("|".join([' {} '.format(i) for i in fret_str]))
+    print('+'.join(['-'*4]*frets))
+
+    # print E (hi)
+    notes = [' '] * frets
+    notes[d] = "D"
+    print("|".join([' {:2s} '.format(note) for note in notes]))
+
+    # print B
+    notes = [' '] * frets
+    notes[a] = "A"
+    notes[d+1] = "D"
+    print("|".join([' {:2s} '.format(note) for note in notes]))
+
+    # print G
+    notes = [' '] * frets
+    notes[a] = "A"
+    notes[e] = "E"
+    notes[d] = "D"
+    print("|".join([' {:2s} '.format(note) for note in notes]))
+    
+    # print D
+    notes = [' '] * frets
+    notes[a] = "A"
+    notes[e+1] = "E"
+    print("|".join([' {:2s} '.format(note) for note in notes]))
+    
+    # print A
+    notes = [' '] * frets
+    notes[e+1] = "E"
+    print("|".join([' {:2s} '.format(note) for note in notes]))
+
+    # print E (lo)
+    notes = [' '] * frets
+    print("|".join([' {:2s} '.format(note) for note in notes]))
+    
+
+def showCaged(root_name):
+    root = (Notes.index(root_name) + 5) % 12 # set for the lo E string
+    c = (root + 4) % 12
+    a = (root + 7) % 12
+    g = (root + 10) % 12
+    e = (root) % 12
+    d = (root + 2) % 12
+    if c == 0: c = 12
+    if a == 0: a = 12
+    if g == 0: g = 12
+    if e == 0: e = 12
+    if d == 0: d = 12
+    frets = max(12, c+3, a+2, g+3, e+2, d+3) + 1
+    print(root_name, root, a, e, d, frets)
+
+    print('CAGED for {}'.format(root_name))
+    fret_str = ["%-2d " % (i, ) for i in range(frets)]
+    fret_str[root] = '{:2} '.format(root_name)
+    print("|".join([' {}'.format(i) for i in fret_str]))
+    print('+'.join(['-'*4]*frets))
+
+    # print E (hi)
+    notes = [[' ']* 2 for i in range(frets)]
+    notes[c][0] = "C"
+    notes[a][0] = "A"
+    notes[g+3][0] = "G"
+    notes[e][0] = "E"
+    if notes[d+2][0] == ' ':
+        notes[d+2][0] = "D"
+    else:
+        notes[d+2][1] = "D"
+    print("|".join([' {}{} '.format(*note) for note in notes]))
+
+    # print B
+    notes = ['  '] * frets
+    notes = [[' ']* 2 for i in range(frets)]
+    notes[c+1][0] = "C"
+    notes[a+2][0] = "A"
+    notes[g][0] = "G"
+    notes[e][0] = "E"
+    if notes[d+3][0] == ' ':
+        notes[d+3][0] = "D"
+    else:
+        notes[d+3][1] = "D"
+    print("|".join([' {}{} '.format(*note) for note in notes]))
+
+    # print G
+    notes = [[' ']* 2 for i in range(frets)]
+    notes[c][0] = "C"
+    notes[a+2][0] = "A"
+    notes[g][0] = "G"
+    notes[e+1][0] = "E"
+    if notes[d+2][0] == ' ':
+        notes[d+2][0] = "D"
+    else:
+        notes[d+2][1] = "D"
+    print("|".join([' {}{} '.format(*note) for note in notes]))
+    
+    # print D
+    notes = [' '] * frets
+    notes = [[' ']* 2 for i in range(frets)]
+    notes[c+2][0] = "C"
+    notes[a+2][0] = "A"
+    notes[g][0] = "G"
+    notes[e+2][0] = "E"
+    if notes[d][0] == ' ':
+        notes[d][0] = "D"
+    else:
+        notes[d][1] = "D"
+    print("|".join([' {}{} '.format(*note) for note in notes]))
+    
+    # print A
+    notes = ['  '] * frets
+    notes = [[' ']* 2 for i in range(frets)]
+    notes[c+3][0] = "C"
+    if notes[a][0] == ' ':
+        notes[a][0] = "A"
+    else:
+        notes[a][1] = "A"
+    notes[g+2][0] = "G"
+    notes[e+2][0] = "E"
+    print("|".join([' {}{} '.format(*note) for note in notes]))
+
+    # print E (lo)
+    notes = ['  '] * frets
+    notes = [[' ']* 2 for i in range(frets)]
+    notes[g+3][0] = "G"
+    notes[e][0] = "E"
+    print("|".join([' {}{} '.format(*note) for note in notes]))
+    
 
 def main():
     '''
@@ -86,6 +227,11 @@ def main():
                         action='store_true', default=False,
                         help='show pentatonic scale '
                         '(default is minor scale; use --major to change)')
+    parser.add_argument('--caged', action='store_true', default=False,
+                        help='show caged for a chord')
+    parser.add_argument('--triads', '--tri', action='store_true',
+                        default=False,
+                        help='show triads for a chord')
     parser.add_argument('notes', type=str, action='store', nargs="*")
 
     args = parser.parse_args()
@@ -196,13 +342,19 @@ def main():
         # no notes given. show all notes
         args.notes = Notes
         print('Fretboard -- All notes\n')
+    elif args.triads:
+        showTriads(args.notes[0].title())
+        exit(0)
+    elif args.caged:
+        showCaged(args.notes[0].title())
+        exit(0)
     else:
         for i in range(len(args.notes)):
             args.notes[i] = args.notes[i].title()
         print('Notes: {}\n'.format(', '.join(args.notes)))
 
     args.frets += 1
-    run(args)
+    showNotes(args)
 
 
 if __name__ == "__main__":
